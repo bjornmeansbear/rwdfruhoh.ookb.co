52ee2d92ff790a65c5592d2d58651f1650634b1f
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-09-06 00:15:43 -0400

    massive theme reset

---
+++ _rwdftheme/javascripts/rwdf.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+  // set common height of triptych blocks on homepage
+  var blockwidth = $('.triptych .span4').width();
+  var blockheight = blockwidth/1.32;
+  $('.triptych .trio').height(blockheight);
+
+  // set slideshow height cleverly
+  var wh = $(window).height();
+  $('#slideshow').height(wh-250);
+
+  //prepare whatwedo feed for isotope...
+  $('.icons.categories .science a').attr('data-filter', '.science');
+  $('.icons.categories .scholars a').attr('data-filter', '.scholars');
+  $('.icons.categories .arts a').attr('data-filter', '.arts');
+  $('.icons.categories .justice a').attr('data-filter', '.justice');
+
+  //isotope stuff
+  var $container = $('#projects');
+  $container.isotope({
+  })
+  $('#filters a').click(function(event){
+    event.preventDefault();
+    var selector = $(this).attr('data-filter');
+    $container.isotope({ filter: selector });
+    return false;
+  });
+  $('.icons.categories a').click(function(event){
+    event.preventDefault();
+    var selector = $(this).attr('data-filter');
+    $container.isotope({ filter: selector });
+    return false;
+  });
+
+});
+
+$(window).bind("load", function() {
+  // make tagline the same height as the lead feature
+  var fh = $('.hero .feature').height();
+  $('.hero .tagline .text').height(fh);
+
+  // subtract height of projects so prev/next buttons show up in right places
+  var containerheight = $('.container.inner').height();
+  var btnheight     = containerheight+30;
+  var relpos        = (containerheight-30)*-1;
+  $('.minus-top .pager a').css('top',relpos);
+  $('.minus-top .pager a').height(btnheight);
+});
+
+//gallery keyboard events
+$(document.documentElement).keyup(function (event) {
+  if (event.keyCode == 37){
+    $('.cycle-slideshow').cycle('prev');
+  }else if (event.keyCode == 39){
+    $('.cycle-slideshow').cycle('next')
+  }
+});
+
+
+//Working on making sidebar navigation follow active trail.
+$("#filters a").click(function() {
+  $("#filters").find('li.active').removeClass('active');
+  $(this).parent().parent().addClass('active');
+});
+
